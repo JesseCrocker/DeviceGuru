7e27bc6f9e340200c79cfd710b7795562a6dc6e3
Jesse Crocker <jesse@datamongers.net>
2025-10-01 08:54:55 -0600

    Revert accidental change

--- Sources/DeviceGuruImplementation+Extension.swift
+++ Sources/DeviceGuruImplementation+Extension.swift
@@ -2,7 +2,7 @@
 public extension DeviceGuruImplementation {
 
     /// This should be same as cocoa pod version
-    static var libraryVersion: String { "<#Major#>.<#Minor#>.<#Fixes#>" }
+    static var libraryVersion: String { "10.0.10" }
 
     var hardware: Hardware {
 
