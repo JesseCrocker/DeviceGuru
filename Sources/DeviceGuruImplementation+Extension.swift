8cb0f28e9f58b6b5ed42344479f32536ad31aeb0
Jesse Crocker <jesse@datamongers.net>
2025-10-01 08:47:59 -0600

    add iPhones 17

--- Sources/DeviceGuruImplementation+Extension.swift
+++ Sources/DeviceGuruImplementation+Extension.swift
@@ -2,7 +2,7 @@
 public extension DeviceGuruImplementation {
 
     /// This should be same as cocoa pod version
-    static var libraryVersion: String { "10.0.10" }
+    static var libraryVersion: String { "<#Major#>.<#Minor#>.<#Fixes#>" }
 
     var hardware: Hardware {
 
@@ -182,6 +182,11 @@ public extension DeviceGuruImplementation {
         if (hardwareString == "iPhone17,2") { return .iphone_16_pro_max }
         if (hardwareString == "iPhone17,3") { return .iphone_16 }
         if (hardwareString == "iPhone17,4") { return .iphone_16_plus }
+        if (hardwareString == "iPhone17,5") { return .iphone_16_e }
+        if (hardwareString == "iPhone18,1") { return .iphone_17_pro }
+        if (hardwareString == "iPhone18,2") { return .iphone_17_pro_max }
+        if (hardwareString == "iPhone18,3") { return .iphone_17 }
+        if (hardwareString == "iPhone18,4") { return .iphone_17_air }
         if (hardwareString == "iPhone2,1") { return .iphone_3gs }
         if (hardwareString == "iPhone3,1") { return .iphone_4 }
         if (hardwareString == "iPhone3,2") { return .iphone_4 }
